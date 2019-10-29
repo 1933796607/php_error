1c20d1e452a791a82d54117ff972b7180a8f625b
jiangjishi <1933796607@qq.com>
2019-10-29 10:48:34 +0800

    自定义错误处理类

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+use Core\Error;
+
+include 'Error.php';
+
+$error = new Error(false);
+$error->error();
+echo $a;
+// echo $a;
+// echo $a;
+// echo $a;
+// echo $a;
+include aaa;
---
+++ views/Error.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <div class="container">
+        <div class="card mt-3">
+            <div class="card-header">
+                Error
+            </div>
+            <div class="card-body">
+                <?php echo $msg; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
